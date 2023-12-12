a6145d10a5e936d7ed035522eabd91e67b79cb4c
Willie Jones <williejones432@yahoo.com>
2023-12-12 16:46:11 -0600

    file path updated

--- develop/app.js
+++ develop/app.js
@@ -8,7 +8,8 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+
 
 
 // connects to files in routes directory
